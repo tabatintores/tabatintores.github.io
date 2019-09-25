eb825c31bb709743d52eb33f1deb6b6403115b13
Kanat Kolbekov <45336538+tabatintores@users.noreply.github.com>
2019-09-25 20:17:21 +0500

    New site

--- js/main.js
+++
@@ -1,9 +0,0 @@
- var quadra = document.querySelector('.quadra');
-
- quadra.addEventListener('click', function() {
- 		var r=Math.floor(Math.random() * 256);
-    var g=Math.floor(Math.random() * 256);
-    var b=Math.floor(Math.random() * 256);
-    var randomColor='#' + r + g + b;
-    quadra.style.cssText = "background-color: " + randomColor; 
- })
---
+++ precache-manifest.b167c3cfbf6e7aa66bd9859e7a0b5057.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5360043a2a3e7983142bd434cb0960d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "912e0270bd4acb1ddf03",
+    "url": "/static/css/main.57a5f1df.chunk.css"
+  },
+  {
+    "revision": "75c195047a1fa4496738",
+    "url": "/static/js/2.b205a101.chunk.js"
+  },
+  {
+    "revision": "912e0270bd4acb1ddf03",
+    "url": "/static/js/main.634f0b47.chunk.js"
+  },
+  {
+    "revision": "d6d5ec26ce9dd4e3a873",
+    "url": "/static/js/runtime-main.6893e38b.js"
+  }
+]);
