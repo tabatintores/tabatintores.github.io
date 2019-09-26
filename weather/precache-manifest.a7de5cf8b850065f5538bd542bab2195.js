d6af09b872ae18f20c789927129175e436c583f6
Kanat Kolbekov <45336538+tabatintores@users.noreply.github.com>
2019-09-27 02:48:00 +0500

    Added weather

---
+++ weather/precache-manifest.a7de5cf8b850065f5538bd542bab2195.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1465d747290e89a6d339322fa65b8be2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2452d4dbff4a4a781046",
+    "url": "/static/css/main.f1cfb511.chunk.css"
+  },
+  {
+    "revision": "ef42ba39e64183775fd2",
+    "url": "/static/js/2.3197a9d7.chunk.js"
+  },
+  {
+    "revision": "2452d4dbff4a4a781046",
+    "url": "/static/js/main.767de354.chunk.js"
+  },
+  {
+    "revision": "aa470f608894b1768938",
+    "url": "/static/js/runtime-main.9897636c.js"
+  }
+]);
