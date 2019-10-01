2b6ae50419f6c897487c3cfa6d25d6153a1c5f6f
Kanat Kolbekov <45336538+tabatintores@users.noreply.github.com>
2019-10-01 22:11:41 +0500

    2

---
+++ todo/precache-manifest.186f29bf9c7c4a5013293353f38583f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17963d7f9fdfd9142c09d9e17c263dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a47669a028f90ab9fa10",
+    "url": "/static/css/main.57a5f1df.chunk.css"
+  },
+  {
+    "revision": "75c195047a1fa4496738",
+    "url": "/static/js/2.b205a101.chunk.js"
+  },
+  {
+    "revision": "a47669a028f90ab9fa10",
+    "url": "/static/js/main.58e0f1ef.chunk.js"
+  },
+  {
+    "revision": "d6d5ec26ce9dd4e3a873",
+    "url": "/static/js/runtime-main.6893e38b.js"
+  }
+]);
