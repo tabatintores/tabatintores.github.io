8d3aa44868a0c7ccfae0c41294e05f8dfe784946
Kanat Kolbekov <45336538+tabatintores@users.noreply.github.com>
2019-10-08 19:15:55 +0500

    Update

---
+++ blog/precache-manifest.fe0c9c3e29c23c56b699521ae9ccf378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9720d41061474f17b78869a0538b5799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9476b00b2d80959c458",
+    "url": "/static/css/main.b4075a43.chunk.css"
+  },
+  {
+    "revision": "ebfa71ec411343c6e298",
+    "url": "/static/js/2.c7cd4f88.chunk.js"
+  },
+  {
+    "revision": "f9476b00b2d80959c458",
+    "url": "/static/js/main.63a07386.chunk.js"
+  },
+  {
+    "revision": "9e6ebaf97ecff88941d0",
+    "url": "/static/js/runtime-main.30278997.js"
+  }
+]);
