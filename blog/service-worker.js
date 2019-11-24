4d4f173d8d4fd11a02b8e589dc2e9d78dfc9b214
Kanat Kolbekov <45336538+tabatintores@users.noreply.github.com>
2019-11-24 23:02:45 +0500

    Update

--- blog/service-worker.js
+++ blog/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "tabatintores.github.io/precache-manifest.cd16514cd4cdbdfec002f3fd063817f0.js"
+  "/precache-manifest.dd4ccb1f85126a995d067243b868a1b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("tabatintores.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
