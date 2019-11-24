8b7c0215b3ba2b7986d33cf30c115458c312b960
Kanat Kolbekov <45336538+tabatintores@users.noreply.github.com>
2019-11-25 01:02:21 +0500

    +

---
+++ todo/precache-manifest.ec98ddeaa1d9bfd725cf86a3d649a661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d25a9c996fd563d457133beb25f1d8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b30331f28c7cde307389",
+    "url": "/todo/static/css/main.57a5f1df.chunk.css"
+  },
+  {
+    "revision": "75c195047a1fa4496738",
+    "url": "/todo/static/js/2.b205a101.chunk.js"
+  },
+  {
+    "revision": "b30331f28c7cde307389",
+    "url": "/todo/static/js/main.b6c605fa.chunk.js"
+  },
+  {
+    "revision": "e4d63df9bc15bb09dcc4",
+    "url": "/todo/static/js/runtime-main.db04e67a.js"
+  }
+]);
--- todo/service-worker.js
+++ todo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.186f29bf9c7c4a5013293353f38583f9.js"
+  "/todo/precache-manifest.ec98ddeaa1d9bfd725cf86a3d649a661.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
