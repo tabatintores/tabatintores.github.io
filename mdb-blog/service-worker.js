1c5b0a2f3b75e42edf40df0e13721268d4ee0c09
Kanat <k.kolbekov@thecoders.ru>
2020-01-14 11:38:49 +0500

    init

--- mdb-blog/service-worker.js
+++ mdb-blog/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-blog/precache-manifest.6c36da3dbf120b50bc0f3ada55827272.js"
+  "/mdb-blog/precache-manifest.693096aa37a6b356e9c3007827768a8b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-blog/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mdb-blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
